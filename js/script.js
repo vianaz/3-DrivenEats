79b3088143f9341467a1e06559a9d82430adcc2c
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-01-26 16:51:21 -0300

    Java Script Implementado (Agora pode selecionar os botões e liberar o botão de fechar pedido)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,49 @@
-function selectPrato (this) {
-    console.log(this);
+// Função para liberar o botão
+function liberarbotao() {
+    let botaoliberado = document.querySelector('.footer-botao');
+    botaoliberado.style.backgroundColor = '#32B72F';
+    botaoliberado.innerHTML = '<p class="roboto">Fechar Pedido</p>'
+} 
+
+// Função Para Escolher o PRATO
+function selectPrato (prato) {
+    let tirar = document.getElementsByClassName('main-conteiner_comida');
+    // Para deselecionar todos os outros pratos
+    for (let i = 0; i < 5; i++) {
+        tirar[i].style.boxShadow = 'none';
+    }
+    prato.style.boxShadow = '0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F';
+    contador1 = 1
+    if (contador1 == contador2 == contador3) {
+        liberarbotao()
+    }
+}
+
+// Função para escolher a Bebida
+function selectBebida (bebida) {
+    let tirar = document.getElementsByClassName('main-conteiner_comida');
+    // Para deselecionar todas outras bebidas
+    for (let i = 4; i < 9; i++) {
+        tirar[i].style.boxShadow = 'none';
+    }
+    bebida.style.boxShadow = '0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F';
+    contador2 = 1
+    if (contador1 == contador2 == contador3) {
+        liberarbotao()
+    }
+}
+
+// Função para escolher a Sobremesa
+function selectSobremesa (sobremesa) {
+    let tirar = document.getElementsByClassName('main-conteiner_comida');
+    // Para deselecionar todas outras sobremesas
+    for (let i = 8; i < 12; i++) {
+        tirar[i].style.boxShadow = 'none';
+    }
+    sobremesa.style.boxShadow = '0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F';
+    console.log(tirar)
+    contador3 = 1
+    if (contador1 == contador2 == contador3) {
+        liberarbotao()
+    }
 }

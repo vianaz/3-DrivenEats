ef5d73dd6bc293041bc2d5313ed5eeb025a31083
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-01-27 16:21:42 -0300

    Tudo feito, só falta o bonus-2

--- js/script.js
+++ js/script.js
@@ -1,49 +1,79 @@
-// Função para liberar o botão
-function liberarbotao() {
-    let botaoliberado = document.querySelector('.footer-botao');
-    botaoliberado.style.backgroundColor = '#32B72F';
-    botaoliberado.innerHTML = '<p class="roboto">Fechar Pedido</p>'
-} 
+// Array vazia para guardar pedidos
+let pedido = [];
+
+let textoMensagem;
+
 
 // Função Para Escolher o PRATO
 function selectPrato (prato) {
-    let tirar = document.getElementsByClassName('main-conteiner_comida');
-    // Para deselecionar todos os outros pratos
-    for (let i = 0; i < 5; i++) {
-        tirar[i].style.boxShadow = 'none';
+    if (typeof pedido[0] === "undefined") {
+        pedido[0] = prato;
+        prato.classList.add("mudar-borda");
+    } else {
+        pedido[0].classList.remove("mudar-borda");
+        pedido[0] = prato;
+        prato.classList.add("mudar-borda");
     }
-    prato.style.boxShadow = '0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F';
-    contador1 = 1
-    if (contador1 == contador2 == contador3) {
-        liberarbotao()
-    }
+    liberarbotao()
 }
 
 // Função para escolher a Bebida
 function selectBebida (bebida) {
-    let tirar = document.getElementsByClassName('main-conteiner_comida');
-    // Para deselecionar todas outras bebidas
-    for (let i = 4; i < 9; i++) {
-        tirar[i].style.boxShadow = 'none';
+    if (typeof pedido[1] === "undefined") {
+        pedido[1] = bebida;
+        bebida.classList.add("mudar-borda");
+    } else {
+        pedido[1].classList.remove("mudar-borda");
+        pedido[1] = bebida;
+        bebida.classList.add("mudar-borda");
     }
-    bebida.style.boxShadow = '0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F';
-    contador2 = 1
-    if (contador1 == contador2 == contador3) {
-        liberarbotao()
-    }
+    liberarbotao()
 }
 
 // Função para escolher a Sobremesa
 function selectSobremesa (sobremesa) {
-    let tirar = document.getElementsByClassName('main-conteiner_comida');
-    // Para deselecionar todas outras sobremesas
-    for (let i = 8; i < 12; i++) {
-        tirar[i].style.boxShadow = 'none';
+    if (typeof pedido[2] === "undefined") {
+        pedido[2] = sobremesa;
+        sobremesa.classList.add("mudar-borda");
+    } else {
+        pedido[2].classList.remove("mudar-borda");
+        pedido[2] = sobremesa;
+        sobremesa.classList.add("mudar-borda");
     }
-    sobremesa.style.boxShadow = '0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F';
-    console.log(tirar)
-    contador3 = 1
-    if (contador1 == contador2 == contador3) {
-        liberarbotao()
+    liberarbotao()
+}
+
+// Função para liberar o botão
+function liberarbotao() {
+    if (typeof pedido[0] != "undefined" && typeof pedido[1] != "undefined" && typeof pedido[2] != "undefined") {
+        pegarDados();
+        let botaoliberado = document.querySelector('.footer-botao');
+        botaoliberado.style.backgroundColor = '#32B72F';
+        botaoliberado.innerHTML = '<p class="roboto">Fechar Pedido</p>';
+        
     }
+} 
+
+// Pega os dados do pedido
+function pegarDados() {
+    // Nomes
+    nomePrato = pedido[0].getElementsByTagName("h3")[0].textContent;
+    nomeBebida = pedido[1].getElementsByTagName("h3")[0].textContent;
+    nomeSobremesa = pedido[2].getElementsByTagName("h3")[0].textContent;
+
+    // Valores
+    valorPrato = pedido[0].getElementsByTagName("span")[0].textContent.replace(',', '.');
+    valorBebida = pedido[1].getElementsByTagName("span")[0].textContent.replace(',', '.');
+    valorSobremesa = pedido[2].getElementsByTagName("span")[0].textContent.replace(',', '.');
+    valorTotal = parseFloat(valorBebida) + parseFloat(valorPrato) + parseFloat(valorSobremesa);
+}
+
+// Link pro Hiroshima e Nagazapi
+function wppMensagem() {
+    // Pegar nome e endereço, monta mensagem
+    nome = prompt('Qual seu nome?');
+    endereco = prompt('Qual seu endereço?');
+    textoMensagem = `Olá, gostaria de fazer o pedido:\n-Prato: ${nomePrato}\n-Bebida: ${nomeBebida}\n-Sobremesa:  ${nomeSobremesa}\nTotal: R$ ${valorTotal}\n\nNome: ${nome}\nEndereço: ${endereco}`;
+    // Abre WPP
+    window.open(`https://wa.me/5598999073330?text=${encodeURIComponent(textoMensagem)}`);
 }
